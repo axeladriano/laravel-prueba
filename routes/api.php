fb0d1b81716cf16caf7474ce582a2bd3df0474eb
unknown <axeladriano>
2024-11-09 13:16:36 -0500

    subiendo

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\EventController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -21,8 +12,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::controller(ReservationController::class)->group(function(){
+    Route::get('/reservation','index');
     Route::get('/reservation/{id}','show');
     Route::post('/reservation','store');
+    Route::delete('/reservation','delete');
 });
 
 Route::controller(EventController::class)->group(function(){
